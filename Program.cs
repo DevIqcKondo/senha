7016b41abfbbe83363efd7b84cb81efab6382221
Isadora Kondo <kondoiqcdev@gmail.com>
2021-07-08 13:00:44 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Figgle;
+
+namespace senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha="1234abcd";
+            string senha2;
+
+            Console.WriteLine("Temos uma mensagem para você! Para visualizá-la informe sua senha!");
+            Console.Write("SENHA: ");
+            senha2=Convert.ToString(Console.ReadLine());
+
+            if(senha2 != senha ){
+                Console.BackgroundColor=ConsoleColor.Red;
+                Console.ForegroundColor=ConsoleColor.Black;
+                Console.WriteLine("ACESSO NEGADO");
+                Console.ResetColor();
+
+            } else{
+                string mensagem ="Você é incrível!";
+                Console.BackgroundColor=ConsoleColor.Green;
+                Console.ForegroundColor=ConsoleColor.Black;
+                Console.WriteLine("ACESSO PERMITIDO!");
+                Console.ResetColor();
+
+                Console.WriteLine("Agora tecle ENTER para exibir a mensagem!");
+                Console.ReadKey();
+                Console.WriteLine(FiggleFonts.Standard.Render(mensagem));
+            }
+
+
+
+            
+
+        }
+    }
+}
